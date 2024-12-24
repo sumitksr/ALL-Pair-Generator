b82d561a526138be334e63c9f55080282c7a7e27
sumitksr <sumitksr6223@gmail.com>
2024-12-24 11:24:20 +0530

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,44 +1,50 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-    void solve(vector<int>nums,vector<vector<int>>&ans,vector<int>output,int index){
-        //base case
-        if(index==nums.size()){
-            ans.push_back(output);
-            return;
-        }
-        //exclude
-        solve(nums,ans,output,index+1);
-        //include
-        int e = nums[index];
-        output.push_back(e);
-        solve(nums,ans,output,index+1);
+void solve(vector<int> nums, vector<vector<int>> &ans, vector<int> output, int index) {
+    // Base case
+    if (index == nums.size()) {
+        ans.push_back(output);
+        return;
     }
-    int main(){
-        vector<vector<int>>ans;
-        vector<int>output;
-        vector<int>nums;
-        int size;
-        cout<<"Size of array: ";
-        cin>>size;
-        cout<<"Input Array: ";
-        for(int i =0;i<size;i++){
-            int x;
-            cin>>x;
-            nums.push_back(x);
-        }
-        solve(nums,ans,output,0);
-        for (const auto& subset : ans) {
+    // Exclude the current element
+    solve(nums, ans, output, index + 1);
+    
+    // Include the current element
+    output.push_back(nums[index]);
+    solve(nums, ans, output, index + 1);
+}
+
+int main() {
+    vector<vector<int>> ans;
+    vector<int> output;
+    vector<int> nums;
+    int size;
+
+    cout << "Size of array: ";
+    cin >> size;
+
+    cout << "Input Array: ";
+    for (int i = 0; i < size; i++) {
+        int x;
+        cin >> x;
+        nums.push_back(x);
+    }
+
+    solve(nums, ans, output, 0);
+
+    // Display subsets
+    for (const auto &subset : ans) {
         cout << "[";
         for (size_t i = 0; i < subset.size(); i++) {
             cout << subset[i];
             if (i != subset.size() - 1) {
-                cout << " ";  
+                cout << " ";
             }
         }
         cout << "]" << endl;
     }
-        cout << "]" << endl;
-    }
-    }
+
+    return 0;
+}
